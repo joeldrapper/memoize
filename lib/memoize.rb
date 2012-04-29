cdfc84a17466b924e28fd372ca46045eb99c48e7
Slava Vishnyakov <vishnyakov@media-saturn.com>
2012-04-29 16:44:02 +0400

    Thread safety

--- lib/memoize.rb
+++ lib/memoize.rb
@@ -1,6 +1,10 @@
+require 'thread'
+
 module Memoize
    # The version of the memoize library
-   MEMOIZE_VERSION = '1.3.1'
+   MEMOIZE_VERSION = '1.3.2'
+   
+   @@lock = Mutex.new
    
    # Memoize the method +name+.  If +file+ is provided, then the method results
    # are stored on disk as well as in memory.
@@ -10,7 +14,9 @@ module Memoize
       (class<<self; self; end).send(:define_method, name) do |*args|
          unless cache.has_key?(args)
             cache[args] = super(*args)
-            File.open(file, 'wb'){ |f| Marshal.dump(cache, f) } if file
+            @@lock.synchronize {
+               File.open(file, 'wb'){ |f| Marshal.dump(cache, f) } if file
+            }
          end
          cache[args]
       end
